8488823f621bbc2321672e701195b96272e62967
AngieCapra <angie_caprarulo@yahoo.com.ar>
2023-11-05 17:00:45 -0300

    cards, hover, carrousel, intento de media queris

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
+//Codigo hecho cuando habia un solo modal.
+
 //abrir el modal
-function abrirModal() {
+/*function abrirModal() {
   let modal = document.getElementById("modal");
   modal.style.display = "block";
 }
@@ -15,7 +17,63 @@ let enlace = document.getElementById("colaborar_form");
 enlace.addEventListener("click", function (event) {
   event.preventDefault();
   abrirModal();
+});*/
+
+// Abrir el primer modal
+function abrirModal_1() {
+  let modal = document.getElementById("modal_1");
+  modal.style.display = "block";
+}
+
+// Cerrar el primer modal
+function cerrarModal_1() {
+  let modal = document.getElementById("modal_1");
+  modal.style.display = "none";
+}
+
+// Abrir el segundo modal
+function abrirModal_2() {
+  let modal = document.getElementById("modal_2");
+  modal.style.display = "block";
+}
+
+// Cerrar el segundo modal
+function cerrarModal_2() {
+  let modal = document.getElementById("modal_2");
+  modal.style.display = "none";
+}
+
+// Abrir el tercer modal
+function abrirModal_3() {
+  let modal = document.getElementById("modal_3");
+  modal.style.display = "block";
+}
+
+// Cerrar el tercer modal
+function cerrarModal_3() {
+  let modal = document.getElementById("modal_3");
+  modal.style.display = "none";
+}
+
+// Eventos para abrir los nuevos modales
+let enlace1 = document.getElementById("colaborar_form");
+enlace1.addEventListener("click", function (event) {
+  event.preventDefault();
+  abrirModal_1();
 });
+
+let enlace2 = document.getElementById("adoptar_form");
+enlace2.addEventListener("click", function (event) {
+  event.preventDefault();
+  abrirModal_2();
+});
+
+let enlace3 = document.getElementById("amadrinar_form");
+enlace3.addEventListener("click", function (event) {
+  event.preventDefault();
+  abrirModal_3();
+});
+
 
 
 
